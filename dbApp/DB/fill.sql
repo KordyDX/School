2d309c2a1d8e6ba89471a0383d0e34d2d650f518
Kristián Stefurak <majk123yt@gmail.com>
2023-03-27 12:26:20 +0200

    Fix

--- dbApp/DB/fill.sql
+++ dbApp/DB/fill.sql
@@ -1,14 +1,14 @@
-INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES ('','Kristián Stefurak','Czech Republic');
-INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES ('','Till Lindeman','Germany');
-INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES ('','George Miller','United States of America');
-INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES ('','Jay Kay','England');
-INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES ('','Nikolas Erlebach','Czech Republic');
-INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES ('','Michal Bubílek','Czech Republic');
+INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES (NULL,'Kristián Stefurak','Czech Republic');
+INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES (NULL,'Till Lindeman','Germany');
+INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES (NULL,'George Miller','United States of America');
+INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES (NULL,'Jay Kay','England');
+INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES (NULL,'Nikolas Erlebach','Czech Republic');
+INSERT INTO `customer`(`idCustomer`, `CustomerName`, `CustomerCountry`) VALUES (NULL,'Michal Bubílek','Czech Republic');
 
-INSERT INTO `order`(`idOrder`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES ('','Renault','Red','2006','1');
-INSERT INTO `order`(`idOrder`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES ('','BMW','White','1986','1');
-INSERT INTO `order`(`idOrder`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES ('','Mecedes Benz','Gray','2011','2');
-INSERT INTO `order`(`idOrder`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES ('','Ford','Blue','2014','3');
-INSERT INTO `order`(`idOrder`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES ('','Lamborghini','Black','1974','4');
-INSERT INTO `order`(`idOrder`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES ('','BMW','Blue','2006','5');
-INSERT INTO `order`(`idOrder`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES ('','Tesla','White','2022','6');
+INSERT INTO `orders`(`idOrders`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES (NULL,'Renault','Red','2006','1');
+INSERT INTO `orders`(`idOrders`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES (NULL,'BMW','White','1986','1');
+INSERT INTO `orders`(`idOrders`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES (NULL,'Mecedes Benz','Gray','2011','2');
+INSERT INTO `orders`(`idOrders`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES (NULL,'Ford','Blue','2014','3');
+INSERT INTO `orders`(`idOrders`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES (NULL,'Lamborghini','Black','1974','4');
+INSERT INTO `orders`(`idOrders`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES (NULL,'BMW','Blue','2006','5');
+INSERT INTO `orders`(`idOrders`, `VehicleName`, `ColorName`, `MakeYear`, `Customer_idCustomer`) VALUES (NULL,'Tesla','White','2022','6');
