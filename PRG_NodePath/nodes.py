b4fd0d29d377ac5cf9565bd181c17e6bb128d090
Kristián Stefurak <76947058+KordyDX@users.noreply.github.com>
2023-10-14 17:30:12 +0200

    Add files via upload

---
+++ PRG_NodePath/nodes.py
@@ -0,0 +1,19 @@
+input = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 3, 6, 0],
+    [0, 3, 0, 4, 0],
+    [0, 6, 4, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+
+def verifyPath(graph, node1, node2):
+    if node1 >= 0 and node2 >= 0:
+        if graph[node1][node2] != 0:
+            return "Path exists"
+        else:
+            return "Path does not exist"
+    else:
+        return "Invalid node"
+
+
+print(verifyPath(input, 3, 4))
